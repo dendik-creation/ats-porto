bb6390a70d08c513dd122e5ffe9f549056b53153
dendik-creation <setiawandendik0205@gmail.com>
2026-09-12 15:44:38 +0700

    fix: adjust landing

--- src/i18n/cv/id.cv.ts
+++ src/i18n/cv/id.cv.ts
@@ -7,7 +7,7 @@ import type { CV } from '../../data/cv';
 export const cv: CV = {
   basics: {
     name: "Dendi' Setiawan",
-    label: 'Software Engineer',
+    label: 'Fullstack Dev',
     email: 'setiawandendik0205@gmail.com',
     url: 'https://dendikcreation.dev',
     cvUrl: 'https://drive.google.com/file/d/1IC-G5b8WJTjGduhHqbUdW04bh5lSSKqL/view?usp=sharing',
